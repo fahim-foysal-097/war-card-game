6b66ce9eb909ef35a1884a8036686dd448aa1f53
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-21 00:31:41 +0600

    Create war_2.py

---
+++ python/war_2.py
@@ -0,0 +1,86 @@
+from war_base import *
+
+
+
+deck = Deck()
+deck.shuffle_deck()
+
+player_1 = Player("One")
+player_2 = Player("Two")
+
+
+# splits deck cards equally
+for _ in range(26):
+    player_1.add_bottom(deck.deal_one())
+    player_2.add_bottom(deck.deal_one())
+
+
+rounds = 0
+game_on = True    
+
+
+while game_on and round_limit > rounds:
+
+    if shuffle_cards_on and rounds > 1:
+        print("Shuffling player cards")
+        shuffle(player_1.hand_cards)
+        shuffle(player_2.hand_cards)
+
+    rounds += 1
+    print(f"Round {rounds}")
+    at_war = True
+
+    if len(player_1.hand_cards) == 0:
+        print(f"{player_1.name} has no cards remaining. {player_2.name} wins the game")
+        game_on = False
+        break
+    elif len(player_2.hand_cards) == 0:
+        print(f"{player_2.name} has no cards remaining. {player_1.name} wins the game")
+        game_on = False
+        break
+
+        
+    player_1_table = []
+    player_2_table = []
+    player_1_table.append(player_1.remove_top())
+    player_2_table.append(player_2.remove_top())
+
+    while at_war:
+            
+        if player_1_table[-1].value > player_2_table[-1].value:
+            print(f"{player_1.name} has won the battle")
+            player_1.add_bottom(player_1_table)
+            player_1.add_bottom(player_2_table)
+            at_war = False
+            break
+
+        elif player_1_table[-1].value < player_2_table[-1].value:
+            print(f"{player_2.name} has won the battle")
+            player_2.add_bottom(player_1_table)
+            player_2.add_bottom(player_2_table)
+            at_war = False
+            break
+
+        else:
+
+            print("WAR!")
+
+            if len(player_1.hand_cards) < 3:
+                print(f"{player_1.name} has no cards to play war. {player_2.name} wins the game")
+                game_on = False
+                break
+
+            elif len(player_2.hand_cards) < 3:
+                print(f"{player_2.name} has no cards to play war. {player_1.name} wins the game")
+                game_on = False
+                break
+
+            else:
+                for _ in range(cards_out):
+                    player_1_table.append(player_1.remove_top())
+                    player_2_table.append(player_2.remove_top())
+else:
+    if round_limit <= 3:
+        print("Out of limit. It's a draw")
+    else:
+        pass
